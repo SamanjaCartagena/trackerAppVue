656343e7197edd7e0fc51bf2824f55599cb483fd
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-18 00:37:26 -0400

    tracker

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+</script>
+
+<template>
+<div id="app">
+  <img v-bind:src={picture} alt="">
+  <h1>Hello {{firstName}}{{lastName}}</h1>
+  <h3>Email:{{email}}</h3>
+  <button>Get random user</button>
+</div>
+</template>
+
+
+<script>
+export default {
+data(){
+  return{
+    firstName:'John',
+    lastname:'Doe',
+    email:'john@gmail.com',
+    gender:'male',
+    picture:'https://randomuser.me/api/portraits/men/10.jpg'
+  }
+}
+
+}
+</script>
+
+<style>
+#app{
+  text-align: center;
+  height:900px;
+  width: 100%;
+  background-color: white;
+  color:black;
+}
+button{
+  background-color: black;
+  color:white;
+  border:1px solid white;
+  padding:10px;
+}
+</style>
+
+<style>
+@import './assets/base.css';
+
+
+</style>
